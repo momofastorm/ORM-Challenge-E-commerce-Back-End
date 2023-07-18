fb776b3de24863808785643b0612f2cf4cd905ff
momofastorm <marcyrappaport.blessed@gmail.com>
2023-07-18 13:58:04 -0500

    first commit

---
+++ models/Tag.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+    },
+    product_name
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
